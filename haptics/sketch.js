6a1fe6f68daf74d957fb06fc579a61433f7217fe
asrodriguez <asrodriguez@gmail.com>
2017-01-29 17:03:12 +0200

    initial upload

---
+++ haptics/sketch.js
@@ -0,0 +1,35 @@
+
+var fadeOut;
+var d_fadeOut;
+
+var gui;
+var prueba;
+var duracion;
+
+function setup() {
+
+  noCanvas(); 
+  // d_fadeOut = select(#d_fadeOut);
+  // fadeOut = select(#fadeOut);
+  // fadeOut.mousePressed(runFadeOut());
+  duracion = createInput();
+
+  prueba = createButton('prueba');
+  prueba.mousePressed(vibrar);
+  
+
+}
+
+
+
+function vibrar(){
+ //createP(duracion.value());
+ Haptics.vibrate(duracion.value());
+//navigator.vibrate([1000, 500, 2000]);
+
+}
+
+// function runFadeOut(){
+//   Haptics.fadeIn([500, 100, 500]);
+
+// }
