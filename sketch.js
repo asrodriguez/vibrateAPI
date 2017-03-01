64fb24d1493653b43f16743a82c7406b3597e94c
Andrés <Andrs>
2017-03-01 17:44:12 -0300

    a

--- sketch.js
+++ sketch.js
@@ -90,30 +90,29 @@ function chooseButton(){
   }
 }
 
-function concatenar(Haptics.heartbeat(1000), Haptics.fadeOut(5000)){
+function concatenar(Haptics.heartbeat(1000), Haptics.fadeOut(5000)) {
   //Haptics.createPattern(Haptics.fadeOut(2000));
   //Haptics.concatenatePatternFuncs(Haptics.heartbeat(1000), Haptics.fadeOut(5000));
   //Haptics.createPattern(Haptics.fadeOut(2000), Haptics.fadeIn(2000),Haptics.heartbeat(3000));
 
 
-            var funcs = arguments;
-            len = arguments.length;
+  var funcs = arguments;
+  var len = arguments.length;
 
-            createP(len);
+  createP(len);
 
-        return function (duration) {
-            var i = 0,
-                d = duration / len;
+  /*return function(duration) {
+    var i = 0,
+      d = duration / len;
 
-            function executeCurrentFunc() {
-                funcs[i](d);
-            }
+    function executeCurrentFunc() {
+      funcs[i](d);
+    }
 
-            for (i = 0; i < len; i += 1) {
-                global.setTimeout(executeCurrentFunc, d);
-            }
-        };
-
+    for (i = 0; i < len; i += 1) {
+      global.setTimeout(executeCurrentFunc, d);
+    }
+  };*/
 
 }
 
